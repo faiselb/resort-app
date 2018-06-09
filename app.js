0a038b1c573b365d59e536b5653c61bed28274bd
faisel-burqawi <faisel.burqawi@gmail.com>
2018-06-09 14:39:12 -0400

    added users controller

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 const indexRouter = require('./routes/index');
-
+const userController = require('./routes/userController');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -12,5 +12,5 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use('/', indexRouter);
-
+app.use('/user', userController);
 module.exports = app;
