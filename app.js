c6479a960b52e2a4eee8b835e1f035537d1cc52c
faisel-burqawi <faisel.burqawi@gmail.com>
2018-06-09 14:27:06 -0400

    Added homepage

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+const indexRouter = require('./routes/index');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', function (req, res) {
-    res.send('resort app test');
-});
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use('/', indexRouter);
 
 module.exports = app;
